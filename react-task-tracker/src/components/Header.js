ae8308fd3ddea6a858c33d96fda76566e99a5b38
James McNeil <mcneil_james@hotmail.com>
2021-01-26 00:17:19 +0000

    Added onClick as prop. Added onClick Handler

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 const Header = ({ title }) => {
+  const onClick = (e) => {
+    console.log('clicked')
+  }
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color='green' text='Add' />
+      <Button color='green' text='Add' onClick={onClick} />
     </header>
   )
 }
